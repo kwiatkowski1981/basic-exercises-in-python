40d1aa2fbb423efbf4829c3b7fd22cd4feb7b902
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-03-21 19:58:23 +0100

    wykonuje cwiczenia zadane przez nauczyciela

--- .idea/basicExercises.py
+++ .idea/basicExercises.py
@@ -1,32 +1,34 @@
-
 from math import sqrt
 
+print(2 * 17)
 
-a = 2
-b = 17
-c = 2 * 17
+a = 66
+b = 5
+c = a % b
+d = a // b
 print(c)
+print(d)
 
-d = 66
-e = 5
-f = d % e
-g = d // e
-print(f)
-print(g)
+print(9 ** 3)
 
-h = 9
-i = 3
-j = h ** i
-print(j)
-
-k = 123321
-l = 11
-m = k / l
-print(int(m))
+print(int(123321 / 11)) # konwersja jawna typu z float na int (pozbywam sie wartosci po przecinku)
 
 print(sqrt(121)) # import  z biblioteki ==>>>  from math import sqrt
 
 print(27 ** (1/3)) # inna forma zapisu wyciągania pierwiastków BEZ importowania z biblioteki funkcji SQRT
 print(9 ** (1/2))   # tak samo jak wyej tylko przyklad do potegi drugiej a nie trzeciej
 
+print(type(123321 / 11)) # sprawdzam typ wyniku wyrazenia
 
+route = 150
+fuelPrice = 5
+fuelUsage = 6
+
+fuelConsumption = (route *  fuelUsage) / 100
+fuelCost = fuelConsumption * fuelPrice
+print(f'zuzycie paliwa na trasie {route}km to {int(fuelConsumption)} litrów i kosztuje nas to {int(fuelCost)}$')
+
+first = 3.14
+second = 3,14
+print(type(first))
+print(type(second))
