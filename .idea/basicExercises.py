7d217a18720747bbf7c0897f83282206698f4f59
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-03-21 18:44:24 +0100

    Initial commit

---
+++ .idea/basicExercises.py
@@ -0,0 +1,29 @@
+
+from math import sqrt
+
+
+a = 2
+b = 17
+c = 2 * 17
+print(c)
+
+d = 66
+e = 5
+f = d % e
+g = d // e
+print(f)
+print(g)
+
+h = 9
+i = 3
+j = h ** i
+print(j)
+
+k = 123321
+l = 11
+m = k / l
+print(int(m))
+
+print(sqrt(121)) # import  z biblioteki ==>>>  from math import sqrt
+
+
